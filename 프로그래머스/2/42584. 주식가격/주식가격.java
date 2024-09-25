287bbcd4043fbaa0f741628248a68021ff173d3f
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-09-25 20:17:34 +0900

    [level 2] Title: 주식가격, Time: 56.20 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,18 +1,35 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int N = prices.length;
         int[] answer = new int[N];
         
-        for(int i=0;i<N;i++){
-            int num = prices[i];
-            for(int j=i+1;j <N; j++){
-                answer[i]++;
-                if(num > prices[j]){
-                    break;
-                }
+        // for(int i=0;i<N;i++){
+        //     int num = prices[i];
+        //     for(int j=i+1;j <N; j++){
+        //         answer[i]++;
+        //         if(num > prices[j]){
+        //             break;
+        //         }
+        //     }
+        // }
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        
+        for(int i=1;i<N;i++){
+            while(!st.isEmpty() && prices[i] < prices[st.peek()]){
+                
+                int topIndex = st.pop();
+                answer[topIndex] = i - topIndex;
             }
+            st.push(i);
+            
         }
         
+        while(!st.isEmpty()){
+            int topIndex = st.pop();
+            answer[topIndex] = N-1-topIndex;
+        }
         
         return answer;
     }
